425d7c0e13a1abaada65ad7a6c31b2f155ee33ed
DeskTabe <you@example.com>
2026-06-02 01:00:38 +0500

    modified:   app.js 	deleted:    generate-google-api-credentials.js

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const {google} = require('googleapis');
+const fs = require('fs');
+// Load variables from .env into process.env
+require('dotenv').config();
 
 const app = express();
 const port = 3300;
+
+
+const credentials = {
+  type: 'service_account',
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: 'googleapis.com'
+};
+
+fs.writeFileSync(
+  'google-api-credentials.json',
+  JSON.stringify(credentials, null, 2)
+);
 
 const auth = new google.auth.GoogleAuth({
   keyFile:'google-api-credentials.json',
@@ -10,13 +33,13 @@ const auth = new google.auth.GoogleAuth({
 })
 
 
-var allowCrossDomain = function(req,res,next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();  
-}
-app.use(allowCrossDomain);
+// var allowCrossDomain = function(req,res,next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();  
+// }
+// app.use(allowCrossDomain);
 
 
 async function  writeToSheet(values) {
